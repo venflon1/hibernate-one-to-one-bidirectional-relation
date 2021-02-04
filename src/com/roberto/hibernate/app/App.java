d5530627ada2fa5d4342a6043f5a1acdcf60f156
Roberto <roberto.amato.11.93@gmail.com>
2021-02-04 15:10:07 +0100

    bidirectional relation one2One

---
+++ src/com/roberto/hibernate/app/App.java
@@ -0,0 +1,36 @@
+package com.roberto.hibernate.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.roberto.hibernate.model.Instructor;
+import com.roberto.hibernate.model.InstructorDetail;
+
+public class App {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = new Configuration()
+												.configure("hibernate.cfg.xml")
+												.addAnnotatedClass(Instructor.class)
+												.addAnnotatedClass(InstructorDetail.class)
+												.buildSessionFactory();
+
+		Session dbSession = sessionFactory.openSession();
+
+		// ================== SELECT RECORD ================
+		Instructor instructor1 = dbSession.get(Instructor.class, 20L);
+		dbSession.close();
+		
+		// BIDIRECTIONAL RELATION  
+		//
+		// [Instructor  -------> DetailInstruct]
+		InstructorDetail detail = instructor1.getInstructorDetail();
+		System.out.println(detail);
+		// [Instructor  <------- DetailInstruct]
+		Instructor instructor = detail.getInstructor();
+		System.out.println(instructor);
+		
+		sessionFactory.close();
+	}
+}
